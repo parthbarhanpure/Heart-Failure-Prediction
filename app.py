287a0dcb06d32b411424cf07b91f75b242f476e6
parthbarhanpure <parthsbarhanpure@gmail.com>
2025-04-05 20:29:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the trained model
+model = pickle.load(open('heart_failure_model.pkl', 'rb'))  # (Sahi)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        # Get data from form
+        age = int(request.form['age'])
+        sex = int(request.form['sex'])
+        cp = int(request.form['cp'])
+        trestbps = int(request.form['trestbps'])
+        chol = int(request.form['chol'])
+        fbs = int(request.form['fbs'])
+        restecg = int(request.form['restecg'])
+        thalach = int(request.form['thalach'])
+        exang = int(request.form['exang'])
+        oldpeak = float(request.form['oldpeak'])
+        slope = int(request.form['slope'])
+        ca = int(request.form['ca'])
+        thal = int(request.form['thal'])
+
+        data = np.array([[age, sex, cp, trestbps, chol, fbs, restecg,
+                          thalach, exang, oldpeak, slope, ca, thal]])
+        
+        prediction = model.predict_proba(data)[0][1] * 100  # Percentage chance
+        
+        return render_template('index.html', prediction_text=f'Heart Failure Chances: {prediction:.2f}%')
+
+if __name__ == '__main__':
+    app.run(debug=True)
