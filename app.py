981f548fdde4b89fe840e6329505a94c99324933
parthbarhanpure <parthsbarhanpure@gmail.com>
2025-04-16 16:31:02 +0530

    updated PORT

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def predict():
         return render_template('index.html', prediction_text=f'Heart Failure Chances: {prediction:.2f}%')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
